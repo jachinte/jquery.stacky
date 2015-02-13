0c220f700dd78cc897f45161dfd8bd1ad25f0e33
jachinte <migueljimenezachinte@gmail.com>
2015-02-13 17:07:16 -0500

    Features added: closing, expanding and collapsing panels. Plugin refactored to follow boilerplate presented in http://stefangabos.ro/jquery/jquery-plugin-boilerplate-oop/

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,103 +1,47 @@
 $(document).ready(function(){
     'use strict';
 
-    var panels = 0;
+    var panels = 0,
+        sizes = ['thin', 'medium', 'wide'];
 
     // Returns a random integer between min (included) and max (excluded)
     function getRandomInt(min, max) {
       return Math.floor(Math.random() * (max - min)) + min;
-    } ;
+    };
 
-    // Closes a panel
-    $("body").on('click', ".close", function(){
-        var panel = $(this).closest(".panel"),
-            hasShadow = panel.hasClass('show-shadow');
-
-        panel.remove();
-        panels--;
-
-        if(hasShadow){
-            $('.panels .panel').last().addClass('show-shadow');
-        }
+    $('#panels').Stacky({
+        fadeInSpeed: 'fast',
+        scrollToSpeed: 300
     });
 
-    // Expands/Collapses a panel
-    $("body").on('click', '.expand, .collapse', function(){
-        var panel = $(this).closest(".panel");
-
-        if($(this).hasClass('expand')){
-            $(this)
-                .removeClass('expand')
-                .addClass('collapse')
-                .attr('title', 'Collapse panel');
-            
-            $(this)
-                .find('i')
-                .removeClass('pe-7s-angle-up')
-                .addClass('pe-7s-angle-down');
-
-            panel.addClass('expanded');
-        }else{
-            $(this)
-                .removeClass('collapse')
-                .addClass('expand')
-                .attr('title', 'Expand panel');
-
-            $(this)
-                .find('i')
-                .removeClass('pe-7s-angle-down')
-                .addClass('pe-7s-angle-up');
-                
-            panel.removeClass('expanded');
-        }
-
-        $('#panels').scrollTo(panel, 300);
-    });
-
-    // Creates a new panel
+    // Binds click to reate new panels
     $("body").on('click', ".open-panel", function(){
-
-        var afterPanel = $(this).closest('.panel'),
-            sizes = ['wide', 'medium', 'thin'];
+        
+        var afterPanel = $(this).closest('.panel');
 
         if(afterPanel.length == 0){
             afterPanel = undefined;
         }
-
-        var panel = $(".panels").stacky({
-                title: 'Title ' + (panels + 1),
-                containerId: 'panels',
-                panelId: 'panel' + (panels + 1),
-                navigation: {
-                    left: [
-                        {
-                            link: '#!',
-                            alt: 'Expand panel',
-                            linkClass: 'expand',
-                            iconClass: 'pe-7s-angle-up big-icon'
-                        }
-                    ],
-                    right: [
-                        {
-                            link: '#!',
-                            alt: 'Open a new panel',
-                            linkClass: 'open-panel',
-                            iconClass: 'pe-7s-angle-right big-icon'
-                        },
-                        {
-                            link: '#!',
-                            alt: 'Close panel',
-                            linkClass: 'close',
-                            iconClass: 'pe-7s-close big-icon'
-                        }
-                    ]
-                },
-                scrollToSpeed: 300,
-                size: sizes[getRandomInt(0, 3)],
-                after: afterPanel
-            });
+        
+        $('#panels').data('Stacky').push({
+            title: 'Title ' + (panels + 1),
+            panelId: 'panel' + (panels + 1),
+            navigation: {
+                left: [
+                    {
+                        link: '#!',
+                        alt: 'Open a new panel',
+                        linkClass: 'open-panel',
+                        iconClass: 'pe-7s-angle-right big-icon'
+                    }
+                ]
+            },
+            size: sizes[getRandomInt(0, 3)],    // Creates panels of random size
+            after: afterPanel
+        });
 
         panels++;
+
     });
 
 });
--- src/js/jquery.stacky.js
+++ src/js/jquery.stacky.js
@@ -1,168 +1,368 @@
-(function ($) {
+;(function ($, window, document, undefined) {
     'use strict';
 
-    /*
-     * This is used to control the z-index property in the panels, in order to
-     * stablish an order of creation, and consequently, show the last added one "on top"
-     * of the others (i.e., show left and right shadow).
-     */
-    var count = 1;
+    var pluginName = 'Stacky';
 
-    /*
-     * Creates the panel structure with the basic content configuration (title, navigation and shadows)
-     */
-    function createPanelStructure(options) {
-        var divStr = '<div></div>',
-            panel = $('<section></section>').addClass($.fn.stacky.classes.panelClass).addClass(options.size),
-            header = $('<header></header>'),
-            navs = createNavs(options),
-            content = $(divStr).addClass($.fn.stacky.classes.contentClass),
-            article = $('<article></article>'),
-            shadowLeft = $(divStr).addClass($.fn.stacky.classes.shadowLeftClass),
-            shadowRight = $(divStr).addClass($.fn.stacky.classes.shadowRightClass);
+    $[pluginName] = function(element, options) {
 
-        if(options.panelContent !== ''){
-            content.append(options.panelContent);
-        }
+        var defaults = {
+            fadeInSpeed: 'fast',
+            scrollToSpeed: 400,
+            panelDefaults: {
+                after: undefined,
+                close: {
+                    show: true,
+                    position: 'right'
+                },
+                maximize: {
+                    show: true,
+                    position: 'right'
+                },
+                navigation: {               
+                    left: [],
+                    right: []
+                },
+                content: '',
+                id: '',
+                size: 'medium', // thin, medium, wide
+                title: ''
+            },
+            texts: {
+                close: 'Close panel',
+                collapse: 'Collapse panel',
+                expand: 'Expand panel'
+            }
+        };
 
-        header.append(navs).append(shadowLeft.add(shadowRight));
+        var classes = {
+            /*
+             * It is added to the panel container the first time a panel is appended,
+             * to indicate that the click event for closing panels has been already 
+             * bound
+             */
+            closeClickBound: 'close-bound',
+            // Used to bind the close action (closing a panel) to a DOM element
+            close: 'close',
+            // The default close icon
+            closeIcon: 'pe-7s-close',
+            /*
+             * It is added to the panel container the first time a panel is appended,
+             * to indicate that the click event for collapsing panels (once they have
+             * been extended) has been already bound
+             */
+            collapseClickBound: 'collapse-bound',
+            /* 
+             * Used to bind the collapse action (returning a panel to its initial width)
+             * to a DOM element
+             */
+            collapse: 'collapse',
+            // The default collapse icon
+            collapseIcon: 'pe-7s-angle-down',
+            // Provides a way to add styles to the panel content area
+            content: 'content',
+            // Truncates large titles
+            ellipsis: 'ellipsis',
+            /*
+             * It is added to the panel container the first time a panel is appended,
+             * to indicate that the click event for expanding panels has been already 
+             * bound
+             */
+            expandClickBound: 'expand-bound',
+            /*
+             * Used to bind the expand action (making a panel to fill its container's
+             * width) to a DOM element
+             */
+            expand: 'expand',
+            // The default expand icon
+            expandIcon: 'pe-7s-angle-up', 
+            // Indicates that a panel has been expanded
+            expanded: 'expanded',
+            // Util class to change the floating property of a DOM element (set to 'left')
+            floatLeft: 'left', 
+            // Util class to change the floating property of a DOM element (set to 'right')
+            floatRight: 'right',
+            // Provides a way to add styles to panels
+            panel: 'panel',
+            // Used to indicate the latest added panel
+            showShadow: 'show-shadow',
+            // Util class to add shadow at the left side of a DOM element
+            shadowLeft: 'shadow-left',
+            // Util class to add shadow at the right side of a DOM element
+            shadowRight: 'shadow-right'
+        };
 
-        if(options.title !== ''){
-            var h1 = $('<h1></h1>').addClass($.fn.stacky.classes.ellipsisClass).text(options.title);
-            header.append(h1);
-        }
+        // current instance
+        var plugin = this;
 
-        article.append(content);
-        panel.append(header).append(article);
+        var $element = $(element),  // reference to the jQuery version of DOM element
+            element = element;      // reference to the actual DOM element
 
-        if(options.panelId !== ''){
-            panel.attr('id', options.panelId);
-        }
+        // this holds the merged default, and user-provided options
+        plugin.settings = {};
 
-        panel.append(shadowLeft.add(shadowRight));
+        // this holds the stacked panels
+        plugin.panels = [];
 
-        return panel;
-    } ;
+        // the "constructor" method that gets called when the object is created
+        plugin.init = function() {
 
-    /*
-     *  Creates the navigation structure (left and right action links in the header)
-     */
-    function createNavs(options) {
-        
-        var navLeft = $('<nav></nav>').addClass($.fn.stacky.classes.leftClass),
-            navRight = $('<nav></nav>').addClass($.fn.stacky.classes.rightClass);
+            // the plugin's final properties are the merged default and
+            // user-provided options (if any)
+            plugin.settings = $.extend(true, {}, defaults, options);
+        };
 
-        if(options.navigation && options.navigation.left){
-            var ul = $('<ul></ul>');
+        // public methods
 
-            for(var i = 0; i < options.navigation.left.length; i++){
-                var elem = options.navigation.left[i];
+        // a public method for pushing new panels
+        plugin.push = function(panelOptions) {
+            var panelSettings = $.extend(true, {}, plugin.settings.panelDefaults, panelOptions),
+                panel = createPanelStructure(panelSettings),
+                container = $element;
 
-                var icon = $('<i></i>').addClass(elem.iconClass);
-                var link = $('<a></a>')
-                    .attr('href', elem.link)
-                    .attr('title', elem.alt)
-                    .addClass(elem.linkClass)
-                    .append(icon);
-                var listItem = $('<li></li>').append(link);
+            // As this is the latest panel, show left and right shadow
+            panel.addClass(classes.showShadow);
+            panel.css('display', 'none').css('z-index', '' + plugin.panels.length);
 
-                ul.append(listItem);
+            /*
+             * Remove shadow in all panels (there is no certainty in which panel has
+             * shadow. It is not always the last one)
+             */
+            container.find('.' + classes.panel)
+                .removeClass(classes.showShadow);
+            
+            /*
+             * Append the panel in the corresponding position: after another panel
+             * or at the end of all panels
+             */
+            if(panelSettings.after !== undefined){
+                panelSettings.after.after(panel);
+
+                // Store new panel in the panels array
+                var previousElemIndex = $.inArray(panelSettings.after, plugin.panels);
+                plugin.panels.splice(previousElemIndex + 1, 0, panel);
+
+            }else{
+                container.append(panel);
+                // Store new panel in the panels array
+                plugin.panels.push(panel);
             }
 
-            navLeft.append(ul);
-        }
+            // Finally, show the recently created panel
+            panel.fadeIn(plugin.settings.fadeInSpeed);
 
-        if(options.navigation && options.navigation.right){
-            var ul = $('<ul></ul>');
+            // Bind click events
+            // close panel
+            container.not('.' + classes.closeClickBound)
+                .addClass(classes.closeClickBound)
+                .on('click', '.' + classes.close, { self: plugin }, closePanel);
 
-            for(var i = 0; i < options.navigation.right.length; i++){
-                var elem = options.navigation.right[i];
+            // expand panel
+            container.not('.' + classes.expandClickBound)
+                .addClass(classes.expandClickBound)
+                .on('click', '.' + classes.expand, { self: plugin }, expandPanel);
 
-                var icon = $('<i></i>').addClass(elem.iconClass);
-                var link = $('<a></a>')
-                    .attr('href', elem.link)
-                    .attr('title', elem.alt)
-                    .addClass(elem.linkClass)
-                    .append(icon);
-                var listItem = $('<li></li>').append(link);
+            // collapse panel
+            container.not('.' + classes.collapseClickBound)
+                .addClass(classes.collapseClickBound)
+                .on('click', '.' + classes.collapse, { self: plugin }, collapsePanel);
 
-                ul.append(listItem);
+            if($.fn.scrollTo){
+                container.scrollTo(panel, plugin.settings.scrollToSpeed);
+            }
+        };
+
+        // private methods
+
+        /*
+         * Creates the panel structure with the basic content configuration 
+         * (title, navigation and shadows)
+         */
+        var createPanelStructure = function(panelSettings) {
+            var divStr = '<div></div>',
+                panel = $('<section></section>').addClass(classes.panel).addClass(panelSettings.size),
+                header = $('<header></header>'),
+                navs = createNavs(panelSettings),
+                content = $(divStr).addClass(classes.content),
+                article = $('<article></article>'),
+                shadowLeft = $(divStr).addClass(classes.shadowLeft),
+                shadowRight = $(divStr).addClass(classes.shadowRight);
+
+            if(panelSettings.content !== ''){
+                content.append(panelSettings.content);
             }
 
-            navRight.append(ul);
-        }
+            header.append(navs).append(shadowLeft.add(shadowRight));
 
-        return navLeft.add(navRight);
-    } ;
+            if(panelSettings.title !== ''){
+                var h1 = $('<h1></h1>').addClass(classes.ellipsis).text(panelSettings.title);
+                header.append(h1);
+            }
 
-    /*
-     * Push a new panel in the panel container. It can be pushed next to an specific element
-     * or at the end.
-     */
-    $.fn.stacky = function (options) {
-        var settings = $.extend({}, $.fn.stacky.defaults, options),
-            panel = createPanelStructure(settings),
-            container = $('#' + settings.containerId);
+            article.append(content);
+            panel.append(header).append(article);
 
-        panel.addClass($.fn.stacky.classes.showShadow);
-        panel.css('display', 'none').css('z-index', '' + count++);  
+            if(panelSettings.id !== ''){
+                panel.attr('id', panelSettings.id);
+            }
 
-        $('#' + settings.containerId + ' .' + $.fn.stacky.classes.panelClass).removeClass($.fn.stacky.classes.showShadow);
-        
-        if(settings.after !== undefined){
-            settings.after.after(panel);
-        }else{
-            container.append(panel);
-        }
+            panel.append(shadowLeft.add(shadowRight));
+            return panel;
+        };
 
-        panel.fadeIn(settings.fadeInSpeed);
+        /*
+         *  Creates the navigation structure (left and right action links in the header)
+         */
+        var createNavs = function(panelSettings) {
+            var navLeft = $('<nav></nav>').addClass(classes.floatLeft),
+                navRight = $('<nav></nav>').addClass(classes.floatRight),
+                leftUl = $('<ul></ul>'),
+                rightUl = $('<ul></ul>');
 
-        if($.fn.scrollTo){
-            container.scrollTo(panel, settings.scrollToSpeed);
-        }
+            // Add the expand/collapse link, if activated
+            if(panelSettings.maximize.show === true 
+                && (panelSettings.maximize.position === 'left'
+                    || panelSettings.maximize.position === 'right')){
 
-        return this;
+                panelSettings.navigation[panelSettings.maximize.position].push({
+                    link: '#!',
+                    alt: plugin.settings.texts.expand,
+                    linkClass: classes.expand,
+                    iconClass: classes.expandIcon + ' big-icon'
+                });
+            }
+
+            // Add the close link, if activated
+            if(panelSettings.close.show === true 
+                && (panelSettings.close.position === 'left'
+                    || panelSettings.close.position === 'right')){
+
+                panelSettings.navigation[panelSettings.close.position].push({
+                    link: '#!',
+                    alt: plugin.settings.texts.close,
+                    linkClass: classes.close,
+                    iconClass: classes.closeIcon + ' big-icon'
+                });
+            }
+
+            // Action links for left nav
+            for(var i = 0; i < panelSettings.navigation.left.length; i++){
+                var elem = panelSettings.navigation.left[i],
+                    icon = $('<i></i>').addClass(elem.iconClass),
+                    link = $('<a></a>')
+                        .attr('href', elem.link)
+                        .attr('title', elem.alt)
+                        .addClass(elem.linkClass)
+                        .append(icon),
+                    listItem = $('<li></li>').append(link);
+                leftUl.append(listItem);
+            }
+
+            navLeft.append(leftUl);
+
+            // Action links for right nav
+            for(var i = 0; i < panelSettings.navigation.right.length; i++){
+                var elem = panelSettings.navigation.right[i],
+                    icon = $('<i></i>').addClass(elem.iconClass),
+                    link = $('<a></a>')
+                        .attr('href', elem.link)
+                        .attr('title', elem.alt)
+                        .addClass(elem.linkClass)
+                        .append(icon),
+                    listItem = $('<li></li>').append(link);
+                rightUl.append(listItem);
+            }
+
+            navRight.append(rightUl);
+
+            // Both navs
+            return navLeft.add(navRight);
+        };
+
+        /*
+         * Event handler for closing panels
+         */
+        var closePanel = function (event) {
+            var self = event.data.self,
+                panel = $(this).closest('.' + classes.panel),
+                hasShadow = panel.hasClass(classes.showShadow);
+
+            // Remove the DOM element
+            panel.remove();
+
+            if(hasShadow){
+                $element.find('.' + classes.panel)
+                    .last()
+                    .addClass(classes.showShadow);
+            }
+        };
+
+        /*
+         *  Event handler for expanding panels
+         */
+        var expandPanel = function (event) {
+            var self = event.data.self,
+                panel = $(this).closest('.' + classes.panel);
+
+            $(this)
+                .removeClass(classes.expand)
+                .addClass(classes.collapse)
+                .attr('title', self.settings.texts.collapse);
+            
+            $(this)
+                .find('i')
+                .removeClass(classes.expandIcon)
+                .addClass(classes.collapseIcon);
+
+            panel.addClass(classes.expanded);
+
+            if($.fn.scrollTo){
+                $element.scrollTo(panel, self.settings.scrollToSpeed);
+            }
+        };
+
+        /*
+         * Event handler for collapsing panels
+         */
+        var collapsePanel = function (event) {
+            var self = event.data.self,
+                panel = $(this).closest('.' + classes.panel);
+
+            $(this)
+                .removeClass(classes.collapse)
+                .addClass(classes.expand)
+                .attr('title', self.settings.texts.expand);
+
+            $(this)
+                .find('i')
+                .removeClass(classes.collapseIcon)
+                .addClass(classes.expandIcon);
+                
+            panel.removeClass(classes.expanded);
+
+            if($.fn.scrollTo){
+                $element.scrollTo(panel, self.settings.scrollToSpeed);
+            }
+        };
+
+        // call the "constructor" method
+        plugin.init();
     };
 
-    /*
-     * Default list of classes used in styles and here
-     */
-    $.fn.stacky.classes = {
-        panelClass: 'panel',
-        hiddenClass: 'hidden',
-        showShadow: 'show-shadow',
-        shadowLeftClass: 'shadow-left',
-        shadowRightClass: 'shadow-right',
-        leftClass: 'left',
-        rightClass: 'right',
-        closeClass: 'close',
-        contentClass: 'content',
-        ellipsisClass: 'ellipsis'
-    };
+     // add the plugin to the jQuery.fn object
+     $.fn[pluginName] = function(options) {
 
-    /*
-     * Settings' default values
-     */
-    $.fn.stacky.defaults = {
-        title: '',
-        containerId: 'body',
-        panelId: '',
-        size: 'medium', // thin, medium, wide
-        scrollToSpeed: 400,
-        fadeInSpeed: 'fast',
-        navigation: {
-            left: [],
-            right: [
-                {
-                    link: '#',
-                    alt: 'Close panel',
-                    linkClass: 'close',
-                    iconClass: 'pe-7s-close big-icon'
-                }
-            ]
-        },
-        panelContent: '',
-        after: undefined
-    };
+        // iterate through the DOM elements we are attaching the plugin to
+        return this.each(function() {
 
-}(jQuery));
+          // if plugin has not already been attached to the element
+          if (undefined == $(this).data(pluginName)) {
+
+              // create a new instance of the plugin
+              // pass the DOM element and the user-provided options as arguments
+              var plugin = new $[pluginName](this, options);
+              $(this).data(pluginName, plugin);
+           }
+        });
+    };
+    
+})(jQuery, window, document);
